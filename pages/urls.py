34b445c2995af1edc19c92cb7a9d6647e180cc22
Samir Sadek <netsamir@gmail.com>
2019-10-20 05:59:38 +0200

    running fine

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import HomePageView
 
 urlpatterns = [
-        path('', views.HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
 ]
 
